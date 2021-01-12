c2ba470385c5a54f978aee9def33caa3bf090b0c
Will Miller <willmiller629@gmail.com>
2021-01-11 22:07:26 -0500

    began auth guard service

---
+++ src/app/services/guards/auth-guard.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService} from '../login/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+
+  constructor(
+    private login: LoginService,
+  ) { }
+
+  canActivate(): boolean{
+    if (!this.login.isAuthenticated()) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+}

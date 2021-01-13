3634ba9a30b26a09a1749d12705193eeafa49aaa
Will Miller <willmiller629@gmail.com>
2021-01-13 18:29:12 -0500

    changes to login/auth

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -19,31 +19,31 @@ export class RegistrationComponent implements OnInit{
 
   postToServer(){
     this.http.post(this.url, this.profileForm.value).subscribe(
-      data=>console.log("success!",data),
-      error=>console.log("couldn't post because",error)
+      data => console.log('success!', data),
+      error => console.log('couldn\'t post because', error)
     );
   }
 
   ngOnInit(): void{
 
   this.profileForm = this.fb.group({
-    firstName: [,Validators.required],
+    firstName: [, Validators.required],
     middleName: [],
-    lastName: [,Validators.required],
-    dateOfBirth: [,Validators.required],
-    socialSecurity: [,Validators.required], 
-    email: [,Validators.required],
-    phoneNumber: [,Validators.required],
+    lastName: [, Validators.required],
+    dateOfBirth: [, Validators.required],
+    socialSecurity: [, Validators.required],
+    email: [, Validators.required],
+    phoneNumber: [, Validators.required],
     address: this.fb.group({
-      firstLine: [,Validators.required],
+      firstLine: [, Validators.required],
       secondLine: [],
-      city: [,Validators.required],
-      state: [,Validators.required],
-      zipcode: [,Validators.required]
+      city: [, Validators.required],
+      state: [, Validators.required],
+      zipcode: [, Validators.required]
     }),
     login: this.fb.group({
-      username: [,Validators.required],
-      password: [,Validators.required]
+      username: [, Validators.required],
+      password: [, Validators.required]
     }),
     accounts: this.fb.array([this.newAccounts()])
     });
@@ -52,47 +52,48 @@ export class RegistrationComponent implements OnInit{
 
   newAccounts(){
     return this.fb.group({
-        accountType:[],
+        accountType: [],
         transactions: this.fb.array([this.newTransaction()])
-    })
+    });
   }
 
   newTransaction(){
     return this.fb.group({
-      transactionAmount:[,Validators.required],
+      transactionAmount: [, Validators.required],
       accounts: this.fb.array([this.newTransactionAcct()])
-    })
+    });
   }
 
   newTransactionAcct(){
     return this.fb.group({
-      accountNumber:[,Validators.required],
-      routingNumber:[,Validators.required],
-    })
+      accountNumber: [, Validators.required],
+      routingNumber: [, Validators.required],
+    });
   }
 
-  //only has 1 element
+  // only has 1 element
   get accountsArray(){
-    return <FormArray>this.profileForm.controls['accounts'];
+    return this.profileForm.controls.accounts as FormArray;
   }
 
-  //only has 1 element hence '0'
+  // only has 1 element hence '0'
   get transactionsArray(){
-    return (<FormArray>this.profileForm.controls['accounts']).at(0).get('transactions')
+    return (this.profileForm.controls.accounts as FormArray).at(0).get('transactions');
   }
 
-  //only has 1 element hence '0'
+  // only has 1 element hence '0'
   get transactionAccountsArray(){
-    return ((<FormArray>this.profileForm.controls['accounts']).at(0).get('transactions') as FormArray).at(0).get('accounts')
+    return ((this.profileForm.controls.accounts as FormArray).at(0).get('transactions') as FormArray).at(0).get('accounts');
   }
 
 
 
+  // tslint:disable-next-line:typedef
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.profileForm.value);
     this.postToServer();
-  
+
   }
 
 
--- src/app/services/guards/auth-guard.guard.ts
+++ src/app/services/guards/auth-guard.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService} from '../login/login.service';
+import {AuthenticationService} from '../authentication/authenticationservice.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,25 @@ export class AuthGuardGuard implements CanActivate {
 
   constructor(
     private login: LoginService,
+    private authenticationService: AuthenticationService,
+    private router: Router
   ) { }
 
-  canActivate(): boolean{
-    if (!this.login.isAuthenticated()) {
-      return false;
-    } else {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.authenticationService.isUserLoggedIn()) {
       return true;
     }
-
+    this.router.navigate(['login']);
+    return false;
   }
+
+  // canActivate(): boolean{
+  //   if (!this.login.isAuthenticated()) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  //
+  // }
 
 }

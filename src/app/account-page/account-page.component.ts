4caf52e91fac77ccfbf1898d6c577949a5ccb003
kppetrick <kppetrick@gmail.com>
2021-01-17 11:50:42 -0500

    formatting

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Account} from '../list-accounts/list-accounts.component';
+import {AccountListService} from '../services/account-list/account-list.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.css']
 })
 export class AccountPageComponent implements OnInit {
-
-  constructor() { }
+account: Account;
+  constructor(private router: Router, public listService: AccountListService) { }
 
   ngOnInit(): void {
+    this.getAccount();
   }
-
+  // tslint:disable-next-line:typedef
+  getAccount()
+  {
+    this.listService.retrieveAccount('Null').subscribe(
+      response => {
+        console.log(response);
+        // @ts-ignore
+        this.account = response; },
+      error => console.log('couldn\'t get because', error)
+    );
+  }
+  // tslint:disable-next-line:typedef
+  navigateToTransactions(url)
+  {
+    this.router.navigate(['transaction', url]);
+  }
 }

871fe6f6943664bc0301de453fe1557905c31731
gunjan <pandya.gunjan@gmail.com>
2021-01-19 07:11:43 -0500

    refresh for transactions fixed

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -24,6 +24,7 @@ export class TransactionHist {
               public transactionAmount: string,
               public transactionBalance: string,
               public transactionDate: string) {}
+
 }
 
 @Component({
